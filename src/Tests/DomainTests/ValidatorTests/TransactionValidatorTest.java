49e522124e5f09ccb921280c777869bb18218897
andreeagoga <andreea.goga@yahoo.com>
2019-04-09 00:13:49 +0300

    javafx+tests

---
+++ src/Tests/DomainTests/ValidatorTests/TransactionValidatorTest.java
@@ -0,0 +1,20 @@
+package Tests.DomainTests.ValidatorTests;
+
+import Domain.Transaction;
+import Domain.TransactionValidator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TransactionValidatorTest {
+
+    @Test
+    void validateTransaction() {
+        TransactionValidator transactionV = new TransactionValidator();
+        Transaction transaction = new Transaction(1,1,1,1,"12.12.2019","12:00");
+        transactionV.validate(transaction);
+        //assertThrows(ExceptionValidatorDomain.class, () -> transactionV.validateTransaction(transaction));
+        assertDoesNotThrow(() -> transactionV.validate(transaction));
+    }
+}
